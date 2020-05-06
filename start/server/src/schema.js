064e000b82b7cce21fe2c8a63e9235cfb4921ef6
XeBor <55808155+XeBor@users.noreply.github.com>
2020-05-06 19:36:44 +0300

    connect REST and SQL data

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -47,6 +47,14 @@ const typeDefs = gql`
   message: String
   launches: [Launch]
   }
+
+  type Launch {
+  id: ID!
+  site: String
+  mission: Mission
+  rocket: Rocket
+  isBooked: Boolean!
+  }
 `;
 
 module.exports = typeDefs;
